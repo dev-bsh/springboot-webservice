9951da45b7df7c7ff633bff0596a055bd0e8ee2d
BSH7 <gamaugi77@naver.com>
2021-11-21 17:47:36 +0900

    [test] jpa audition 테스트

--- src/test/java/com/bang/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/bang/springboot/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -46,6 +47,30 @@ public class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2021,11,21,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> all = postsRepository.findAll();
+
+        //then
+        Posts posts = all.get(0);
+        System.out.println("################create_date = "+posts.getCreatedDate());
+        System.out.println("################modified_date = "+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
+
+
+    }
 
 
 
